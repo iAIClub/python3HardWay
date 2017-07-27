98022ec4bb2594e62c4a637cff87fba1d90bf566
Mark Lotspaih <lotspaih@users.noreply.github.com>
2017-07-27 06:37:37 -0400

    Exercise 3
    Core exercise and study drill file.

---
+++ ex3.py
@@ -0,0 +1,35 @@
+# Prints a string.
+print('I will now count my chickens:')
+
+# Prints a string and then performs math operations returning the answer.
+print('Hens', 25.0 + 30.0 / 6.0)
+print('Roosters', 100.0 - 25.0 * 3.0 % 4.0)
+
+# Prints a string
+print('Now I will count the eggs:')
+
+# Performs math operations and returns the answer.
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+# Prints a string
+print('Is it true that 3 + 2 < 5 - 7?')
+
+# Performs math operations that resolve to True or False.
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+# Prints a string and then performs math operations that resolve to
+# True or False.
+print('What is 3 + 2?', 3.0 + 2.0)
+print('What is 5 - 7?', 5.0 - 7.0)
+
+# Prints a string.
+print("Oh, that's why it's False.")
+
+# Prints another string.
+print('How about some more.')
+
+# Prints a string and then performs math operations that resolve to
+# True or False.
+print('Is it greater?', 5.0 > -2.0)
+print('Is it greater or equal?', 5.0 >= -2.0)
+print('Is it less or equal?', 5.0 <= -2.0)
---
+++ ex3NewCalculate.py
@@ -0,0 +1,4 @@
+# Exercise 3 Something to Calculate
+# Calculates the Age of America
+print('The age of the United States of America is:')
+print(1008.5 * 2 - 3552 / 2)
