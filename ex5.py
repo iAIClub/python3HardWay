68f025fcd014b6b5e3fb4598b645f70f51f326c3
Mark Lotspaih <lotspaih@users.noreply.github.com>
2017-08-29 08:55:26 -0400

    Incomplete

--- ex5.py
+++ ex5.py
@@ -1,1 +1,9 @@
 my_name = 'Zed A, Shaw'
+my_age = 35  # Not a lie
+my_height = 74  # inches
+my_weight = 180  # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
