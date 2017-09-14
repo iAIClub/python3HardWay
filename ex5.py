5dcbe3cf496b966728ac17f8440da0e6e85104e4
Mark Lotspaih <lotspaih@users.noreply.github.com>
2017-09-14 10:14:19 -0400

    Exercise 5
    Exercise 5 Completed Study Drills

--- ex5.py
+++ ex5.py
@@ -1,9 +1,24 @@
-my_name = 'Zed A, Shaw'
-my_age = 35  # Not a lie
-my_height = 74  # inches
-my_weight = 180  # lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Zed A, Shaw'
+age = 35  # Not a lie
+height = 74  # inches
+# Convert inches to centimeters by multiplying the inches by 2.54
+# 1 inch = 2.54 centimeters
+height_centimeters = height * 2.54  # height in centimeters
+weight = 180  # lbs
+# Convert lbs to kilograms by multiplying lbs by 0.45
+# 1 kg = 2.2 lbs
+weight_kilograms = weight * 0.45  # kg
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
 
-print(f'Let us talk about {my_name}.')
+print(f'Let us talk about {name}.')
+print(f"He's {height} inches tall or {height_centimeters} centimeters tall.")
+print(f"He's {weight} pounds or {weight_kilograms} kilograms heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+
+# This line is tricky, try to get it exactly right
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
