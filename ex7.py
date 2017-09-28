dbd040fef04ce4c89ca0b46d792b3dc86bff414f
Mark Lotspaih <lotspaih@users.noreply.github.com>
2017-09-28 12:40:25 -0400

    Exercise 7
    Completed

--- ex7.py
+++ ex7.py
@@ -1,21 +1,39 @@
+# Print string to standard screen output
 print("Mary had a little lamb.")
+# Print string to standard screen output and format the {} with the word 'snow'
 print("It's fleece was white as {}.".format('snow'))
+# Print string to standard screen output
 print("And everywhere that Mary went.")
+# Print string to standard screen output ten times in a row
 print("." * 10)  # what did that do?
 
+# Assign variable 'end1' eqaul to 'C'
 end1 = "C"
+# Assign variable 'end2' eqaul to 'h'
 end2 = "h"
+# Assign variable 'end3' eqaul to 'e'
 end3 = "e"
+# Assign variable 'end4' eqaul to 'e'
 end4 = "e"
+# Assign variable 'end5' eqaul to 's'
 end5 = "s"
+# Assign variable 'end6' eqaul to 'e'
 end6 = "e"
+# Assign variable 'end7' eqaul to 'B'
 end7 = "B"
+# Assign variable 'end8' eqaul to 'u'
 end8 = "u"
+# Assign variable 'end9' eqaul to 'r'
 end9 = "r"
+# Assign variable 'end10' eqaul to 'g'
 end10 = "g"
+# Assign variable 'end11' eqaul to 'e'
 end11 = "e"
+# Assign variable 'end12' eqaul to 'r'
 end12 = "r"
 
+# Print the 'end#' variables to standard screen output joining them together
+# for a single line string adding a space between the lines.
 # watch that comma at the end. try removing it to see what happens
 print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
 print(end7 + end8 + end9 + end10 + end11 + end12)
