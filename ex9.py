6b01da22b62616eb0c4142d7cd56e97c91596dea
Mark Lotspaih <lotspaih@users.noreply.github.com>
2017-10-16 12:50:46 -0400

    Exercise 9

--- ex9.py
+++ ex9.py
@@ -1,11 +1,17 @@
 # Here's some new strange stuff, remember type it exactly.
 
+# Assign string of 3 letter days to the days variable
 days = "Mon Tue Wed Thur Fri Sat Sun"
+# Assign 3 letter months string to months variable using the \n to create a
+# new line in between each month
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
+# Print the days and month variables at the end of each string
 print("Here are the days: ", days)
 print("Here are the months: ", months)
 
+# Print the multi-line string using three quotes to let Python know it is a
+# multi-line string.
 print("""
 There's something going on here.
 With the three double-quotes.
