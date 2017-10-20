a42232e6225c634471697ec9819797126ca8053e
Mark Lotspaih <lotspaih@users.noreply.github.com>
2017-10-20 09:10:59 -0400

    Exercise 10

--- ex10.py
+++ ex10.py
@@ -1,14 +1,18 @@
+# Assign strings with escape sequences to variables
 tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 
-fat_cat = """
+# Added additional escape sequences and format strings
+fishy = "Fishies"
+fat_cat = '''
 I'll do a list:
-\t* Cat Food
-\t* Fishies
-\t* Catnip\n\t* Grass
-"""
+\t\b\b\b* Cat Food
+\t\b\b* {}
+\t\b* Catnip\n\t* Grass
+'''.format(fishy)
 
+# Print the various variables to see their formating with escape sequences
 print(tabby_cat)
 print(persian_cat)
 print(backslash_cat)
