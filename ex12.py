9f55d5e2270e12c1f97a89c37ab1b03a72dfd52b
Mark Lotspaih <lotspaih@users.noreply.github.com>
2017-11-10 09:56:58 -0500

    Exercise 12

--- ex12.py
+++ ex12.py
@@ -1,5 +1,9 @@
+# Get string from standard input and assign it to a variable
 age = input("How old are you? ")
 height = input("How tall are you? ")
 weight = input("How much do you weigh? ")
 
+# Print string inserting variables into the string where shown
 print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+# Use pydoc command to view various documentation modules
