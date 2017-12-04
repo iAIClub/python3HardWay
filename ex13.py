70e0eeeeb0a726382d7c0ed20ec0492cce309aea
Mark Lotspaih <lotspaih@users.noreply.github.com>
2017-12-04 13:59:52 -0500

    Exercise 13

--- ex13.py
+++ ex13.py
@@ -1,8 +1,15 @@
+# Imports argv from the sys module
 from sys import argv
-# read the WYSS section for how to run this
+
+# Below assigns the argv CLI input into three variables
 script, first, second, third = argv
 
+# Get name input from user and assign to name variables
+name = input("What is your name? ")
+
+# Print the three argv variables and the single input variable
 print("The script is called:", script)
 print("The first variable is:", first)
 print("The second variable is:", second)
 print("The third variable is:", third)
+print("Your name is: ", name)
