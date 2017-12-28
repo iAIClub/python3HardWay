8ff742379b3b65d1458fa20c06076984e6539186
Mark Lotspaih <lotspaih@users.noreply.github.com>
2017-12-28 14:56:08 -0500

    Incomplete

---
+++ ex16.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+script, filename = argv
+
+print(f"We're going to erase {filename}.")
+print("If you don't want that, hit CTRL-C (^C).")
+print("If you do want that, hit RETURN.")
+
+input("?")
